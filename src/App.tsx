61ba0d7ca52aedfcfccb24637daf9243812de325
Alessandro DESSI <alessandro.dessi@alten.com>
2021-06-19 18:42:45 +0200

    first draft with the weapon buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rock Paper Scissors</h1>
+      {/* eslint-disable-next-line react/button-has-type */}
+      <button
+        onClick={() => {
+          console.log("Your choice: rock");
+        }}
+      >
+        <img style={{ width: "100%" }} src="/img/weapon/rock.png" alt="ROCK" />
+      </button>
+      {/* eslint-disable-next-line react/button-has-type */}
+      <button
+        onClick={() => {
+          console.log("Your choice: paper");
+        }}
+      >
+        <img
+          style={{ width: "100%" }}
+          src="/img/weapon/paper.png"
+          alt="PAPER"
+        />
+      </button>
+      {/* eslint-disable-next-line react/button-has-type */}
+      <button
+        onClick={() => {
+          console.log("Your choice: scissors");
+        }}
+      >
+        <img
+          style={{ width: "100%" }}
+          src="/img/weapon/scissors.png"
+          alt="SCISSORS"
+        />
+      </button>
     </div>
   );
 }
