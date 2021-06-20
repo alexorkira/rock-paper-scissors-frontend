b651381ba2d39fd22db039bb6510a47813f75784
Alessandro DESSI <alessandro.dessi@alten.com>
2021-06-20 11:26:30 +0200

    automtic page title according wepon pack

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import "./App.css";
 import WeaponButton from "./components/weapon/weaponButton";
 import WeaponEnum from "./components/weapon/weapon.enum";
 
+function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function App(): React.ReactElement {
   return (
     <div className="App">
-      <h1>Rock Paper Scissors</h1>
+      <h1>
+        {Object.values(WeaponEnum).map((weaponName) => {
+          return `${capitalizeFirstLetter(weaponName)} `;
+        })}
+      </h1>
       <div className="weaponPack">
         {Object.values(WeaponEnum).map((weaponName) => {
           return <WeaponButton key={weaponName} weaponName={weaponName} />;
